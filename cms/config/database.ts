67d5ac030ed4ada076d9c3eae3e04da20ea8ae65
Jonathan Dupré <419946+kareniel@users.noreply.github.com>
2022-11-26 22:15:32 -0500

    fetch secrets

--- cms/config/database.ts
+++ cms/config/database.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 
-export default ({ env }) => ({
+const dev = ({ env }) => ({
   connection: {
     client: 'sqlite',
     connection: {
@@ -9,3 +9,21 @@ export default ({ env }) => ({
     useNullAsDefault: true,
   },
 });
+
+const prod = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "strapi_db.local"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi_db"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", ""),
+    },
+    useNullAsDefault: true,
+  },
+});
+
+export default process.env.NODE_ENV === 'production'
+  ? prod
+  : dev
